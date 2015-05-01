d32f79ead9dd8d08c98c652415f71dba909bfbeb
tastycode <thomas.devol@gmail.com>
2015-05-01 10:33:47 -0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Since.create(phrase: "The police killed a black male", slug: "police-execution", date: Chronic.parse("4/19/15"), hashtag: "freddiegray", link: "http://www.google.com")
+Since.create(
+  phrase: "The police killed a black male",
+  slug: "police-execution",
+  date: Chronic.parse("4/19/15"),
+  hashtag: "freddiegray",
+  link: "http://www.cnn.com/2015/04/30/us/baltimore-freddie-gray-death-investigation/",
+  description: "Freddie Gray's Death Ruled a Homicide, Officers Face Charges"
+)
